b78db63901f8475977e20359f5b606cf8f4ded03
Ian Rose <ianrosedev@gmail.com>
2017-10-09 15:55:12 -0400

    Made NavBar stateful

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,75 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
 
-const NavBar = () => {
-  const burgerToggle = () => {
-    const narrowLinks = document.querySelector('.nav-narrow-links');
-
-    if (narrowLinks.style.display === 'block') {
-      narrowLinks.style.display = 'none';
-    } else {
-      narrowLinks.style.display = 'block';
-    }
+class NavBar extends Component {
+  state = {
+    isDropdownOpen: false
   };
 
-  return (
-    <nav className='nav-bar'>
-      <div className='nav-wide'>
-        <ul>
-          <li onClick={burgerToggle}>
-            <NavLink exact to='/' activeClassName='selected'>
-              Home
-            </NavLink>
-          </li>
-          <li onClick={burgerToggle}>
-            <NavLink to='/projects' activeClassName='selected'>
-              Projects
-            </NavLink>
-          </li>
-          <li onClick={burgerToggle}>
-            <NavLink to='/about' activeClassName='selected'>
-              About
-            </NavLink>
-          </li>
-          <li onClick={burgerToggle}>
-            <NavLink to='/contact' activeClassName='selected'>
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-      <div className='nav-narrow'>
-        <i
-          className='fa fa-bars'
-          onClick={burgerToggle}
-        >
-        </i>
-        <ul className='nav-narrow-links'>
-          <li onClick={burgerToggle}>
-            <NavLink exact to='/' activeClassName='selected'>
-              Home
-            </NavLink>
-          </li>
-          <li onClick={burgerToggle}>
-            <NavLink to='/projects' activeClassName='selected'>
-              Projects
-            </NavLink>
-          </li>
-          <li onClick={burgerToggle}>
-            <NavLink to='/about' activeClassName='selected'>
-              About
-            </NavLink>
-          </li>
-          <li onClick={burgerToggle}>
-            <NavLink to='/contact' activeClassName='selected'>
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+  burgerClose = () => (
+    this.setState({
+      isDropdownOpen: false
+    })
   );
-};
+
+  burgerToggle = () => (
+    this.setState((prevState) => ({
+      isDropdownOpen: !prevState.isDropdownOpen
+    }))
+  );
+
+  render () {
+    const links = (
+      <span>
+        <li onClick={this.burgerClose}>
+          <NavLink exact to='/' activeClassName='selected'>
+            Home
+          </NavLink>
+        </li>
+        <li onClick={this.burgerClose}>
+          <NavLink to='/projects' activeClassName='selected'>
+            Projects
+          </NavLink>
+        </li>
+        <li onClick={this.burgerClose}>
+          <NavLink to='/about' activeClassName='selected'>
+            About
+          </NavLink>
+        </li>
+        <li onClick={this.burgerClose}>
+          <NavLink to='/contact' activeClassName='selected'>
+            Contact
+          </NavLink>
+        </li>
+      </span>
+    );
+
+    return (
+      <nav className='nav-bar'>
+        <div className='nav-wide'>
+          <ul>
+            {links}
+          </ul>
+        </div>
+        <div className='nav-narrow'>
+          <i
+            className='fa fa-bars'
+            onClick={this.burgerToggle}
+          >
+          </i>
+          <ul
+            className='nav-narrow-links'
+            style={this.state.isDropdownOpen ? { display: 'block' } : { display: 'none' }}
+          >
+            {links}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default NavBar;
